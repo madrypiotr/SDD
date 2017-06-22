e14b6803ab550495e6b0015d9585a6c1c37b96f6
Piotr Mądry <m@drypiotr.pl>
2017-06-22 19:12:10 +0200

    Korekta SMTP

--- server/smtp.js
+++ server/smtp.js
@@ -1,3 +1,3 @@
 Meteor.startup(function(){
-   process.env.MAIL_URL = "smtp://m@drypiotr.pl :smaga111@smtp.drypiotr.pl:587"
+   process.env.MAIL_URL = "smtp://m@drypiotr.pl :smaga111@smtp.mail.drypiotr.pl:587"
 });
