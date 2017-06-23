c1feee4b338c8752b90d8469b5293bb5330b14e9
Piotr Mądry <m@drypiotr.pl>
2017-06-24 00:30:34 +0200

    Test ustawień SMTP (3)

--- server/smtp.js
+++ server/smtp.js
@@ -1,3 +1,3 @@
 Meteor.startup(function(){
-   process.env.MAIL_URL = "smtp://m@drypiotr.pl :smaga111SMAGA111@smtp.drypiotr.pl:587"
+   process.env.MAIL_URL = "smtp://systemadtestowy@gmail.com :smaga111SMAGA111@smtp.gmail.com:587"
 });
