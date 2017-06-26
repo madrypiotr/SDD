3801b614c0797671ee955ca3f2c733e69592c616
Piotr Mądry <m@drypiotr.pl>
2017-06-26 17:51:06 +0200

    Test ViewMap

--- client/views/layout/mainpage.js
+++ client/views/layout/mainpage.js
@@ -30,6 +30,6 @@ Template.mainpage.events({
             notificationPauseWarning("kwestii",kwestiaCanBeInserted);
     },
     'click #wievMap': function () {
-            Router.go("map");
+            Router.go("addKwestia");
 	}
 });
