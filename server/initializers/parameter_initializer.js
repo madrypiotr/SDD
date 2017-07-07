9e31e018bd2c4963175fba00f7d0a44bcaf6e304
Piotr Mądry <m@drypiotr.pl>
2017-07-07 23:11:38 +0200

    Reorganization of the form (4)

--- server/initializers/parameter_initializer.js
+++ server/initializers/parameter_initializer.js
@@ -2,13 +2,13 @@ Meteor.startup(function () {
     //console.log("ZMIANA_PARAMS");
     var globalParameters = [
         {
-            "nazwaOrganizacji": TXV.ORG_NAME,
-            "terytorium": TXV.TERITORY,
-            "terytAdres": TXV.TERITADR,
-            "terytCODE": TXV.TERITCOD,
-            "terytCity": TXV.TERITCITY,
-            "kontakty": TXV.CONTACTS,
-            "regulamin": txv.MISSING_RULES,
+            "nazwaOrganizacji": TAPi18n.__('txv.ORG_NAME'),
+            "terytorium": TAPi18n.__('txv.TERITORY'),
+            "terytAdres": TAPi18n.__('txv.TERITADR'),
+            "terytCODE": TAPi18n.__('txv.TERITCOD'),
+            "terytCity": TAPi18n.__('txv.TERITCITY'),
+            "kontakty": TAPi18n.__('txv.CONTACTS'),
+            "regulamin": TAPi18n.__('txv.MISSING_RULES'),
             "czasWyczekiwaniaKwestiiSpecjalnej":1,
             "voteQuantity":3,
             "voteDuration":1,
