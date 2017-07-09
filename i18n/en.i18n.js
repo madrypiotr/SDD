92c6863450c4cdd6f4855047ae13ef7cfc28944c
Piotr Mądry <m@drypiotr.pl>
2017-07-09 16:39:31 +0200

    Added support for i18n messages

---
+++ i18n/en.i18n.js
@@ -0,0 +1,92 @@
+i18n.map("en", {
+    resetPasswordDialog: {
+        title: "e-Resetuj hasło",
+        newPassword: "e-Nowe hasło",
+        cancel: "e-Anuluj",
+        submit: "e-Ustaw hasło"
+    },
+    enrollAccountDialog: {
+        title: "e-Wybierz hasło",
+        newPassword: "e-Nowe hasło",
+        cancel: "e-Zamknij",
+        submit: "e-Ustaw hasło"
+    },
+    justVerifiedEmailDialog: {
+        verified: "e-Adres email został zweryfikowany",
+        dismiss: "e-Odrzuć"
+    },
+    loginButtonsMessagesDialog: {
+        dismiss: "e-Odrzuć"
+    },
+    loginButtonsLoggedInDropdownActions: {
+        password: "e-Zmień hasło",
+        signOut: "e-Wyloguj się"
+    },
+    loginButtonsLoggedOutDropdown: {
+        signIn: "e-Zaloguj się",
+        up: "e-Zarejestruj się"
+    },
+    loginButtonsLoggedOutPasswordServiceSeparator: {
+        or: "e-lub"
+    },
+    loginButtonsLoggedOutPasswordService: {
+        create: "e-Stwórz",
+        signIn: "e-Zaloguj się ",
+        forgot: "e-Nie pamiętasz hasła?",
+        createAcc: "e-Utwórz konto"
+    },
+    forgotPasswordForm: {
+        email: "e-Email",
+        reset: "e-Resetuj hasło",
+        invalidEmail: "e-Niepoprawny email"
+    },
+    loginButtonsBackToLoginLink: {
+        back: "e-Anuluj"
+    },
+    loginButtonsChangePassword: {
+        submit: "e-Zmień hasło",
+        cancel: "e-Anuluj"
+    },
+    loginButtonsLoggedOutSingleLoginButton: {
+        signInWith: "e-Zaloguj się przez",
+        configure: "e-Configure"
+    },
+    loginButtonsLoggedInSingleLogoutButton: {
+        signOut: "e-Wyloguj się"
+    },
+    loginButtonsLoggedOut: {
+        noLoginServices: "e-Nie skonfigurowano usługi logowania"
+    },
+    loginFields: {
+        usernameOrEmail: "e-Nazwa użytkownika lub email",
+        username: "e-Nazwa użytkownika",
+        email: "e-Email",
+        password: "e-Hasło"
+    },
+    signupFields: {
+        username: "e-Nazwa użytkownika",
+        email: "e-Email",
+        emailOpt: "e-Email (opcjonalnie)",
+        password: "e-Hasło",
+        passwordAgain: "e-Hasło (powtórz)"
+    },
+    changePasswordFields: {
+        currentPassword: "e-Obecne hasło",
+        newPassword: "e-Nowe hasło",
+        newPasswordAgain: "e-Nowe hasło (powtórz)"
+    },
+    infoMessages : {
+        emailSent: "e-Wysłano email",
+        passwordChanged: "e-Hasło zostało zmienione"
+    },
+    errorMessages: {
+        userNotFound: "e-Nie znaleziono użytkownika",
+        invalidEmail: "e-Niepoprawny email",
+        incorrectPassword: "e-Niepoprawne hasło",
+        usernameTooShort: "e-Nazwa użytkowika powinna mieć przynajmniej 3 znaki",
+        passwordTooShort: "e-Hasło powinno składać się przynajmnej z 6 znaków",
+        passwordsDontMatch: "e-Hasło są różne",
+        newPasswordSameAsOld: "e-Nowe hasło musi się różnić od starego",
+        signupsForbidden: "e-Rejstracja wyłączona"
+    }
+});
---
+++ i18n/fr.i18n.js
@@ -0,0 +1,92 @@
+i18n.map("en", {
+    resetPasswordDialog: {
+        title: "f-Resetuj hasło",
+        newPassword: "f-Nowe hasło",
+        cancel: "f-Anuluj",
+        submit: "f-Ustaw hasło"
+    },
+    enrollAccountDialog: {
+        title: "f-Wybierz hasło",
+        newPassword: "f-Nowe hasło",
+        cancel: "f-Zamknij",
+        submit: "f-Ustaw hasło"
+    },
+    justVerifiedEmailDialog: {
+        verified: "f-Adres email został zweryfikowany",
+        dismiss: "f-Odrzuć"
+    },
+    loginButtonsMessagesDialog: {
+        dismiss: "f-Odrzuć"
+    },
+    loginButtonsLoggedInDropdownActions: {
+        password: "f-Zmień hasło",
+        signOut: "f-Wyloguj się"
+    },
+    loginButtonsLoggedOutDropdown: {
+        signIn: "f-Zaloguj się",
+        up: "f-Zarejestruj się"
+    },
+    loginButtonsLoggedOutPasswordServiceSeparator: {
+        or: "f-lub"
+    },
+    loginButtonsLoggedOutPasswordService: {
+        create: "f-Stwórz",
+        signIn: "f-Zaloguj się ",
+        forgot: "f-Nie pamiętasz hasła?",
+        createAcc: "f-Utwórz konto"
+    },
+    forgotPasswordForm: {
+        email: "f-Email",
+        reset: "f-Resetuj hasło",
+        invalidEmail: "f-Niepoprawny email"
+    },
+    loginButtonsBackToLoginLink: {
+        back: "f-Anuluj"
+    },
+    loginButtonsChangePassword: {
+        submit: "f-Zmień hasło",
+        cancel: "f-Anuluj"
+    },
+    loginButtonsLoggedOutSingleLoginButton: {
+        signInWith: "f-Zaloguj się przez",
+        configure: "f-Configure"
+    },
+    loginButtonsLoggedInSingleLogoutButton: {
+        signOut: "f-Wyloguj się"
+    },
+    loginButtonsLoggedOut: {
+        noLoginServices: "f-Nie skonfigurowano usługi logowania"
+    },
+    loginFields: {
+        usernameOrEmail: "f-Nazwa użytkownika lub email",
+        username: "f-Nazwa użytkownika",
+        email: "f-Email",
+        password: "f-Hasło"
+    },
+    signupFields: {
+        username: "f-Nazwa użytkownika",
+        email: "f-Email",
+        emailOpt: "f-Email (opcjonalnie)",
+        password: "f-Hasło",
+        passwordAgain: "f-Hasło (powtórz)"
+    },
+    changePasswordFields: {
+        currentPassword: "f-Obecne hasło",
+        newPassword: "f-Nowe hasło",
+        newPasswordAgain: "f-Nowe hasło (powtórz)"
+    },
+    infoMessages : {
+        emailSent: "f-Wysłano email",
+        passwordChanged: "f-Hasło zostało zmienione"
+    },
+    errorMessages: {
+        userNotFound: "f-Nie znaleziono użytkownika",
+        invalidEmail: "f-Niepoprawny email",
+        incorrectPassword: "f-Niepoprawne hasło",
+        usernameTooShort: "f-Nazwa użytkowika powinna mieć przynajmniej 3 znaki",
+        passwordTooShort: "f-Hasło powinno składać się przynajmnej z 6 znaków",
+        passwordsDontMatch: "f-Hasło są różne",
+        newPasswordSameAsOld: "f-Nowe hasło musi się różnić od starego",
+        signupsForbidden: "f-Rejstracja wyłączona"
+    }
+});
