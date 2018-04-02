6100ac5b413f0b05d5feb99b8fa002eda4630325
Piotr Mądry <m@drypiotr.pl>
2018-04-02 23:45:35 +0200

    Addition of translation panel in admin mode (4)

--- client/views/admin/jezyki/trans_language.js
+++ client/views/admin/jezyki/trans_language.js
@@ -5,3 +5,31 @@ Template.transLanguage.events({
     }
 });
 
+
+Template.transLanguage.helpers({
+	
+    languageCount: function () {
+        return Languages.find().count();
+    },
+    languages: function () {
+        return Languages.find({}).fetch();
+		
+		
+   },
+    language0: function (id) {
+        return Languages.findOne({_id: 0});
+    },
+	
+    language1: function () {
+        return Languages.find(1);
+    },
+	
+    language2: function () {
+        return Languages.find(2);
+    },
+	
+    language3: function () {
+        return Languages.find(3);
+    }
+	
+});
