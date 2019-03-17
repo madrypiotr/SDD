f6ed34f7d2daec9727e17ee70512df1dbae05f8d
Michał Wanat <michalwanat@gmail.com>
2019-03-17 14:40:12 +0100

    SMTP fix

--- server/smtp.js
+++ server/smtp.js
@@ -2,4 +2,5 @@ Meteor.startup(function () {
     if (Meteor.isDevelopment) {
         process.env.MAIL_URL = 'smtp://systemadtestowy@gmail.com :smaga111SMAGA111@smtp.gmail.com:587';
     }
+    console.info(process.env.MAIL_URL);
 });
